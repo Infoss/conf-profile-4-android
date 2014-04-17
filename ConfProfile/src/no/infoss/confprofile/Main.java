1aca241619fc99805853d0c72f150ae584e2b478
Dmitry Vorobiev <vorobievds@gmail.com>
2014-04-17 07:14:26 +0400

    [+] Added content (.mobileconfig) handlers for browser to manifest [~] Small changes in the main activity

--- ConfProfile/src/no/infoss/confprofile/Main.java
+++ ConfProfile/src/no/infoss/confprofile/Main.java
@@ -1,10 +1,12 @@
 package no.infoss.confprofile;
 
+import no.infoss.confprofile.crypto.CertificateManager;
 import no.infoss.confprofile.fragment.AddProfileFragment;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,20 +22,22 @@ public class Main extends Activity {
 		if(intent != null) {
 			if(Intent.ACTION_VIEW.equals(intent.getAction())) {
 				Fragment addProfile = getFragmentManager().findFragmentById(R.id.popupFragmentPanel);
-				if(addProfile == null) {
+				if(addProfile == null || !(addProfile instanceof AddProfileFragment)) {
 					addProfile = new AddProfileFragment();
-					FragmentTransaction ft = getFragmentManager().beginTransaction();
-					ft.replace(R.id.popupFragmentPanel, addProfile);
-					ft.commit();
-					
-					View popupPanel = findViewById(R.id.popupFragmentPanel);
-					popupPanel.setVisibility(View.VISIBLE);
 				}
+				
+				FragmentTransaction ft = getFragmentManager().beginTransaction();
+				ft.replace(R.id.popupFragmentPanel, addProfile);
+				ft.commit();
+				
+				View popupPanel = findViewById(R.id.popupFragmentPanel);
+				popupPanel.setVisibility(View.VISIBLE);
 				
 				((AddProfileFragment) addProfile).parseDataByUri(this, intent.getData());
 			}
-		}
-		
+			
+			CertificateManager.getManager(this, CertificateManager.MANAGER_INTERNAL);
+		}		
 	}
 
 }
