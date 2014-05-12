72679c6ce486f71f6795a186024026d3f3af6af8
Dmitry Vorobiev <vorobievds@gmail.com>
2014-05-12 14:49:14 +0400

    [+] additional scep error codes for tasks

--- ConfProfile/src/no/infoss/confprofile/task/TaskError.java
+++ ConfProfile/src/no/infoss/confprofile/task/TaskError.java
@@ -6,4 +6,8 @@ public interface TaskError {
 	public static final int NETWORK_FAILED = 2;
 	public static final int HTTP_FAILED = 3;
 	public static final int INVALID_DATA = 4;
+	
+	public static final int SCEP_FAILED = 5;
+	public static final int SCEP_TIMEOUT = 6;
+	public static final int MISSING_SCEP_PAYLOAD = 7;
 }
