5825254047c330201e9360c84b636204c94239c0
Dmitry Vorobiev <vorobievds@gmail.com>
2014-05-29 15:47:26 +0400

    [~] small improvements, nothing special

--- ConfProfile/src/no/infoss/confprofile/vpn/VpnTunnel.java
+++ ConfProfile/src/no/infoss/confprofile/vpn/VpnTunnel.java
@@ -2,18 +2,21 @@ package no.infoss.confprofile.vpn;
 
 import java.util.Map;
 
+import no.infoss.confprofile.vpn.VpnManagerService.VpnConfigInfo;
 import android.content.Context;
 
 public abstract class VpnTunnel implements Runnable {
 	
 	protected Thread mThread;
 	protected Context mCtx;
+	protected VpnConfigInfo mCfg;
 	protected long mVpnServiceCtx; //native
 	protected long mVpnTunnelCtx; //native
 	
-	public VpnTunnel(Context ctx) {
+	public VpnTunnel(Context ctx, VpnConfigInfo cfg) {
 		mThread = new Thread(this);
 		mCtx = ctx;
+		mCfg = cfg;
 	}
 	
 	protected abstract String getThreadName();
--- ConfProfile/src/no/infoss/confprofile/vpn/VpnTunnelFactory.java
+++ ConfProfile/src/no/infoss/confprofile/vpn/VpnTunnelFactory.java
@@ -1,15 +1,16 @@
 package no.infoss.confprofile.vpn;
 
+import no.infoss.confprofile.vpn.VpnManagerService.VpnConfigInfo;
 import android.content.Context;
 
 public class VpnTunnelFactory {
-	public VpnTunnel getTunnel(Context ctx, VpnManagerService mgr, String type) {
+	public static VpnTunnel getTunnel(Context ctx, VpnManagerService mgr, VpnConfigInfo cfg) {
 		VpnTunnel tunnel = null;
 		
-		if(OpenVpnTunnel.VPN_TYPE.equals(type)) {
+		if(OpenVpnTunnel.VPN_TYPE.equals(cfg.vpnType)) {
 			RouterLoop routerLoop = mgr.getRouterLoop();
 			if(routerLoop != null) {
-				tunnel = new OpenVpnTunnel(ctx, routerLoop.getRouterCtx(), mgr);
+				tunnel = new OpenVpnTunnel(ctx, routerLoop.getRouterCtx(), mgr, cfg);
 			}
 		}
 		
