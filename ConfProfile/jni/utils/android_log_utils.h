1e47df84aae1429745de81ea937aa3d14cfad8af
Dmitry Vorobiev <vorobievds@gmail.com>
2014-06-05 20:49:39 +0400

    [+] added 2 error levels for logging

--- ConfProfile/jni/utils/android_log_utils.h
+++ ConfProfile/jni/utils/android_log_utils.h
@@ -12,8 +12,10 @@
 
 #define LOGV(LOG_TAG, ...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #define LOGD(LOG_TAG, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(LOG_TAG, ...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGW(LOG_TAG, ...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(LOG_TAG, ...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGF(LOG_TAG, ...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__)
 
 inline void log_dump_packet(const char *tag, uint8_t* buff, int len);
 
