addb0ae34546719d67d62249f437ac8d3b4de26c
Dmitry Vorobiev <vorobievds@gmail.com>
2014-06-10 12:17:26 +0400

    [~] ProfileInfo to ProfileInfoEx conversion extracted into Loader (corresponding AsyncTask was removed)

--- ConfProfile/src/no/infoss/confprofile/fragment/ProfileDetailsFragment.java
+++ ConfProfile/src/no/infoss/confprofile/fragment/ProfileDetailsFragment.java
@@ -16,12 +16,14 @@ import no.infoss.confprofile.profile.BaseQueryCursorLoader;
 import no.infoss.confprofile.profile.DbOpenHelper;
 import no.infoss.confprofile.profile.PayloadsCursorLoader;
 import no.infoss.confprofile.profile.PayloadsCursorLoader.PayloadInfo;
+import no.infoss.confprofile.profile.PayloadsCursorLoader.PayloadsPerformance;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
 import android.content.Loader;
 import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -39,7 +41,7 @@ import com.litecoding.classkit.view.ExpandableObjectAdapter;
 import com.litecoding.classkit.view.LazyCursorList;
 import com.litecoding.classkit.view.ObjectAdapter.ObjectMapper;
 
-public class ProfileDetailsFragment extends Fragment implements LoaderCallbacks<Cursor>  {
+public class ProfileDetailsFragment extends Fragment implements LoaderCallbacks<List<List<PayloadInfoEx>>>  {
 	public static final String TAG = ProfileDetailsFragment.class.getSimpleName(); 
 	
 	private static final String FSK_NAME = TAG.concat(":name");
@@ -139,73 +141,30 @@ public class ProfileDetailsFragment extends Fragment implements LoaderCallbacks<
 	}
 
 	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle params) {
-		return new PayloadsCursorLoader(getActivity(), id, params, mDbHelper);
+	public Loader<List<List<PayloadInfoEx>>> onCreateLoader(int id, Bundle params) {
+		return new PayloadInfoExLoader(getActivity(), id, params, mDbHelper);
 	}
 
 	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-		final LazyCursorList<PayloadInfo> payloadInfoList = 
-				new LazyCursorList<PayloadInfo>(PayloadsCursorLoader.PAYLOAD_CURSOR_MAPPER);
-		payloadInfoList.populateFrom(data, true);
+	public void onLoadFinished(Loader<List<List<PayloadInfoEx>>> loader, List<List<PayloadInfoEx>> data) {
+		Activity activity = getActivity();
 		
-		AsyncTask<Void, Void, List<List<PayloadInfoEx>>> convertTask = 
-				new AsyncTask<Void, Void, List<List<PayloadInfoEx>>>() {
-
-			@Override
-			protected List<List<PayloadInfoEx>> doInBackground(Void... params) {
-				GsonBuilder gsonBuilder = new GsonBuilder();
-				gsonBuilder.registerTypeAdapterFactory(new PlistTypeAdapterFactory());
-				Gson gson = gsonBuilder.create();
-				
-				Map<String, List<PayloadInfoEx>> converted = new HashMap<String, List<PayloadInfoEx>>();
-				for(PayloadInfo info : payloadInfoList) {
-					PayloadInfoEx infoEx = new PayloadInfoEx(info);
-					try {
-						infoEx.payload = PayloadFactory.createPayload(gson.fromJson(info.data, Dictionary.class));
-						List<PayloadInfoEx> list = converted.get(infoEx.payload.getPayloadType());
-						if(list == null) {
-							list = new LinkedList<PayloadInfoEx>();
-							converted.put(infoEx.payload.getPayloadType(), list);
-						}
-						
-						list.add(infoEx);
-					} catch (ConfigurationProfileException e) {
-						Log.e(TAG, "Invalid payload data", e);
-					} catch (JsonSyntaxException e) {
-						Log.e(TAG, "Invalid json", e);
-					}
-				}
-				
-				List<List<PayloadInfoEx>> result = new LinkedList<List<PayloadInfoEx>>();
-				result.addAll(converted.values());
-				converted.clear();
-				return result;
-			}
-			
-			@Override
-			protected void onPostExecute(List<List<PayloadInfoEx>> result) {
-				Activity activity = getActivity();
-				
-				if(activity != null) {
-					ExpandableListAdapter payloadAdapter = new ExpandableObjectAdapter<PayloadInfoEx, List<PayloadInfoEx>>(
-							activity.getLayoutInflater(), 
-							result, 
-							android.R.layout.simple_list_item_2, 
-							new PayloadInfoMapper(), 
-							result, 
-							android.R.layout.simple_expandable_list_item_2, 
-							new PayloadGroupInfoMapper());
-					ExpandableListView list = (ExpandableListView) getView().findViewById(R.id.list);
-					list.setAdapter(payloadAdapter);
-				}
-			}	
-		};
-		convertTask.execute(new Void[0]);
+		if(activity != null) {
+			ExpandableListAdapter payloadAdapter = new ExpandableObjectAdapter<PayloadInfoEx, List<PayloadInfoEx>>(
+					activity.getLayoutInflater(), 
+					data, 
+					android.R.layout.simple_list_item_2, 
+					new PayloadInfoMapper(), 
+					data, 
+					android.R.layout.simple_expandable_list_item_2, 
+					new PayloadGroupInfoMapper());
+			ExpandableListView list = (ExpandableListView) getView().findViewById(R.id.list);
+			list.setAdapter(payloadAdapter);
+		}
 	}
 
 	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
+	public void onLoaderReset(Loader<List<List<PayloadInfoEx>>> loader) {
 		// nothing to do here
 	}
 
@@ -251,14 +210,67 @@ public class ProfileDetailsFragment extends Fragment implements LoaderCallbacks<
 		
 	}
 	
-	private static class PayloadInfoEx extends PayloadInfo {
-		public Payload payload;
-		
-		public PayloadInfoEx(PayloadInfo info) {
-			this.profileId = info.profileId;
-			this.payloadUuid = info.payloadUuid;
-			this.data = info.data;
+	private static class PayloadInfoExLoader extends AsyncTaskLoader<List<List<PayloadInfoEx>>> {
+		protected int mId;
+		protected DbOpenHelper mDbHelper;
+		protected PayloadsPerformance mPerformance;
+
+		public PayloadInfoExLoader(Context ctx, int id, Bundle params, DbOpenHelper dbHelper) {
+			super(ctx);
+			mId = id;
+			mDbHelper = dbHelper;
+			
+			mPerformance = new PayloadsPerformance(getContext(), mId, params, mDbHelper);
 		}
+
+		@Override
+		public List<List<PayloadInfoEx>> loadInBackground() {
+			Cursor dbResult = mPerformance.perform();
+			
+			final LazyCursorList<PayloadInfo> payloadInfoList = 
+					new LazyCursorList<PayloadInfo>(PayloadsCursorLoader.PAYLOAD_CURSOR_MAPPER);
+			payloadInfoList.populateFrom(dbResult, true);
+			
+			GsonBuilder gsonBuilder = new GsonBuilder();
+			gsonBuilder.registerTypeAdapterFactory(new PlistTypeAdapterFactory());
+			Gson gson = gsonBuilder.create();
+			
+			Map<String, List<PayloadInfoEx>> converted = new HashMap<String, List<PayloadInfoEx>>();
+			for(PayloadInfo info : payloadInfoList) {
+				PayloadInfoEx infoEx = new PayloadInfoEx(info);
+				try {
+					infoEx.payload = PayloadFactory.createPayload(gson.fromJson(info.data, Dictionary.class));
+					List<PayloadInfoEx> list = converted.get(infoEx.payload.getPayloadType());
+					if(list == null) {
+						list = new LinkedList<PayloadInfoEx>();
+						converted.put(infoEx.payload.getPayloadType(), list);
+					}
+					
+					list.add(infoEx);
+				} catch (ConfigurationProfileException e) {
+					Log.e(TAG, "Invalid payload data", e);
+				} catch (JsonSyntaxException e) {
+					Log.e(TAG, "Invalid json", e);
+				}
+			}
+			
+			List<List<PayloadInfoEx>> result = new LinkedList<List<PayloadInfoEx>>();
+			result.addAll(converted.values());
+			converted.clear();
+			
+			return result;
+		}
+		
 	}
 
+}
+
+class PayloadInfoEx extends PayloadInfo {
+	public Payload payload;
+	
+	public PayloadInfoEx(PayloadInfo info) {
+		this.profileId = info.profileId;
+		this.payloadUuid = info.payloadUuid;
+		this.data = info.data;
+	}
 }
