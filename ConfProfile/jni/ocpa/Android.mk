fe01c021a5e59091fa15c7008bf05b586c9ac31f
Dmitry Vorobiev <vorobievds@gmail.com>
2014-06-19 15:34:30 +0400

    [-] don't require libocpautils

--- ConfProfile/jni/ocpa/Android.mk
+++ ConfProfile/jni/ocpa/Android.mk
@@ -2,10 +2,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := ocpa
-LOCAL_SRC_FILES := ocpa.c android_jni.c router.c iputils.c tun.c tun_l2tp.c tun_openvpn.c
+LOCAL_SRC_FILES := \
+	ocpa.c \
+	android_jni.c \
+	router.c \
+	iputils.c \
+	tun.c \
+	tun_l2tp.c \
+	tun_openvpn.c \
+	pcap_output.c \
+	android_log_utils.c
 LOCAL_C_INCLUDES := $(ocpa_INCLUDES)
 LOCAL_LDLIBS := -llog 
-LOCAL_STATIC_LIBRARIES := libocpautils libuip
+LOCAL_STATIC_LIBRARIES := libuip
 
 include $(BUILD_SHARED_LIBRARY)
 
