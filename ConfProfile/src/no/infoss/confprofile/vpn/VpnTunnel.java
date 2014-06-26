f4fbfc45ce5f9cddfc7c4585b54b87366025f5a8
Dmitry Vorobiev <vorobievds@gmail.com>
2014-06-26 14:15:45 +0400

    [+] special logging to VpnTunnel (log report w/o any sensitive data to sd card for troubleshooting)

--- ConfProfile/src/no/infoss/confprofile/vpn/VpnTunnel.java
+++ ConfProfile/src/no/infoss/confprofile/vpn/VpnTunnel.java
@@ -1,13 +1,30 @@
 package no.infoss.confprofile.vpn;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
+import no.infoss.confprofile.BuildConfig;
+import no.infoss.confprofile.util.MiscUtils;
 import no.infoss.confprofile.util.PcapOutputStream;
 import no.infoss.confprofile.vpn.VpnManagerService.VpnConfigInfo;
 import android.content.Context;
+import android.util.Log;
 
 public abstract class VpnTunnel implements Runnable {
+	public static final int LOG_VERBOSE = 2;
+	public static final int LOG_DEBUG = 3;
+	public static final int LOG_INFO = 4;
+	public static final int LOG_WARN = 5;
+	public static final int LOG_ERROR = 6;
+	public static final int LOG_FATAL = 7;
 	
+	protected final String mInstanceLogTag;
+	protected final Logger mLogger;
 	protected Thread mThread;
 	protected Context mCtx;
 	protected VpnConfigInfo mCfg;
@@ -18,6 +35,9 @@ public abstract class VpnTunnel implements Runnable {
 		mThread = new Thread(this);
 		mCtx = ctx;
 		mCfg = cfg;
+		
+		mInstanceLogTag = String.format("%s (id=%d)", getClass().getSimpleName(), mThread.getId());
+		mLogger = new Logger();
 	}
 	
 	protected abstract String getThreadName();
@@ -83,4 +103,118 @@ public abstract class VpnTunnel implements Runnable {
 	
 	private native void debugRestartPcap(long vpnTunnelCtx, PcapOutputStream pos);
 	private native void debugStopPcap(long vpnTunnelCtx);
+	
+	private final void debugLogMessage(int level, String msg) {
+		switch (level) {
+		case LOG_FATAL: 
+		case LOG_ERROR: {
+			Log.e(mInstanceLogTag, msg);
+			break;
+		}
+		case LOG_WARN: {
+			Log.w(mInstanceLogTag, msg);
+			break;
+		}
+		case LOG_INFO: {
+			Log.i(mInstanceLogTag, msg);
+			break;
+		}
+		case LOG_DEBUG: {
+			Log.d(mInstanceLogTag, msg);
+			break;
+		}
+		case LOG_VERBOSE: {
+			Log.v(mInstanceLogTag, msg);
+			break;
+		}
+		default: {
+			//silently drop this message
+			break;
+		}
+		}
+	}
+	
+	/*package*/ class Logger {
+		private static final String LOG_FMT = "[%s] [level=%d] %s\n";
+		
+		private boolean mIsInitialized = false;
+		private boolean mIsDeactivated = false;
+		private SimpleDateFormat mFmt = new SimpleDateFormat("HH:mm:ss.SSS");
+		private File mLogFile;
+		private OutputStream mOs;
+		
+		private synchronized void init() {
+			if(mIsInitialized || mIsDeactivated) {
+				return;
+			}
+			
+			try {
+				if(!MiscUtils.isExternalStorageWriteable()) {
+					Log.e(mInstanceLogTag, "Logger is unable to write external storage (storage is RO or unmounted");
+					mIsDeactivated = true;
+					return;
+				}
+				
+				File externalFilesDir = VpnTunnel.this.getContext().getExternalFilesDir(null);
+				if(externalFilesDir == null) {
+					//error: storage error
+					Log.e(mInstanceLogTag, "Logger is unable to write external storage (storage error)");
+					return;
+				}
+				
+				String logFileName = String.format("report-%d-(%s).log", 
+						System.currentTimeMillis(), 
+						VpnTunnel.this.getTunnelId());
+				mLogFile = new File(externalFilesDir, logFileName);
+				FileOutputStream fos = new FileOutputStream(mLogFile);
+				mOs = new BufferedOutputStream(fos, 128 * 1024);
+				
+				String headerFmt = "Current date is %s\n";
+				SimpleDateFormat dateFmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+				mOs.write(String.format(headerFmt, dateFmt.format(new Date())).getBytes("UTF-8"));
+				
+				mIsInitialized = true;
+			} catch(Exception e) {
+				Log.e(mInstanceLogTag, "Can't init logger", e);
+				close();
+			}
+		}
+		
+		public synchronized void log(int level, String data) {
+			if(BuildConfig.DEBUG) {
+				VpnTunnel.this.debugLogMessage(level, data);
+			}
+			
+			if(mIsDeactivated) {
+				return;
+			}
+			
+			if(!mIsInitialized) {
+				init();
+			}
+			
+			if(!mIsInitialized) {
+				return;
+			}
+			
+			try {
+				synchronized(mFmt) {
+					String logMsg = String.format(LOG_FMT, mFmt.format(new Date()), level, data);
+					mOs.write(logMsg.getBytes("UTF-8"));
+				}
+			} catch(Exception e) {
+				Log.e(mInstanceLogTag, "Can't log a message, closing a logger", e);
+				close();
+			}
+			
+			if(BuildConfig.DEBUG) {
+				VpnTunnel.this.debugLogMessage(level, data);
+			}
+		}
+		
+		public void close() {
+			mIsInitialized = false;
+			mIsDeactivated = true;
+		}
+	}
 }
