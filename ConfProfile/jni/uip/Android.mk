c8d301b9d19b8b31e939c0e466eb49f8d9245eb3
Dmitry Vorobiev <vorobievds@gmail.com>
2014-06-30 07:13:11 +0400

    [-] removed uip, trying lwip instead of it

--- ConfProfile/jni/uip/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := uip
-LOCAL_SRC_FILES := uip.c uiplib.c uip_arp.c uip-fw.c uip-neighbor.c uip-split.c psock.c timer.c clock-arch.c
-
-include $(BUILD_STATIC_LIBRARY)
