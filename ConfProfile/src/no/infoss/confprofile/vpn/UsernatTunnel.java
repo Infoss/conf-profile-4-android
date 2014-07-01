e4434eb7daec6258cec16f40027d0de4dc680d18
Dmitry Vorobiev <vorobievds@gmail.com>
2014-07-01 17:23:53 +0400

    [+] usernat tunnel implementation (mostly copied fro openvpn tunnel)

--- ConfProfile/src/no/infoss/confprofile/vpn/UsernatTunnel.java
+++ ConfProfile/src/no/infoss/confprofile/vpn/UsernatTunnel.java
@@ -1,9 +1,19 @@
 package no.infoss.confprofile.vpn;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
+import no.infoss.confprofile.util.MiscUtils;
+import no.infoss.confprofile.vpn.OpenVpnTunnel.VpnStatus;
 import no.infoss.confprofile.vpn.VpnManagerService.VpnConfigInfo;
 import android.content.Context;
+import android.net.LocalServerSocket;
+import android.net.LocalSocket;
+import android.net.LocalSocketAddress;
+import android.util.Log;
 
 public class UsernatTunnel extends VpnTunnel {
 	public static final String TAG = UsernatTunnel.class.getSimpleName();
@@ -12,33 +22,147 @@ public class UsernatTunnel extends VpnTunnel {
 	static {
 		VPN_CFG_INFO = new VpnConfigInfo();
 	}
+	
+	private VpnManagerInterface mVpnMgr;
+	private Thread mWorkerThread;
+	private UsernatWorker mWorker;
+	
+	private LocalSocket mSocket;
+	private LocalServerSocket mServerSocket;
+	private LocalSocket mServerSocketLocal;
 
-	public UsernatTunnel(Context ctx) {
+	public UsernatTunnel(Context ctx, RouterLoop routerLoop, VpnManagerInterface vpnMgr) {
 		super(ctx, VPN_CFG_INFO);
-		// TODO Auto-generated constructor stub
+		mVpnServiceCtx = routerLoop.getRouterCtx();
+		mVpnMgr = vpnMgr;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		if(!MiscUtils.writeExecutableToCache(mCtx, UsernatWorker.USERNAT)) {
+			Log.e(TAG, "Error writing usernat");
+			terminateConnection();
+			return;
+		}
+		
+		File cacheDir = mCtx.getCacheDir();
+		String[] argv = new String[] { cacheDir.getAbsolutePath() + "/" + UsernatWorker.USERNAT };
+		
+		// Could take a while to open connection
+        int tries=8;
 
+        String socketName = (new File(mCtx.getCacheDir(), "mgmtsocket")).getAbsolutePath();
+        // The mServerSocketLocal is transferred to the LocalServerSocket, ignore warning
+
+        mServerSocketLocal = new LocalSocket();
+
+        while(tries > 0) {
+        	if(mConnectionStatus == ConnectionStatus.TERMINATED) {
+        		return;
+        	}
+        	
+            try {
+                mServerSocketLocal.bind(new LocalSocketAddress(socketName,
+                        LocalSocketAddress.Namespace.FILESYSTEM));
+                Log.d(TAG, "unix socket bound at " + socketName);
+                break;
+            } catch (IOException e) {
+                // wait 300 ms before retrying
+            	Log.w(TAG, e);
+                try {
+                	Thread.sleep(300);
+                } catch (InterruptedException e1) {
+                }
+            }
+            tries--;
+        }
+        
+        try {
+            mServerSocket = new LocalServerSocket(mServerSocketLocal.getFileDescriptor());
+            Log.d(TAG, "Management interface opened");
+        } catch (IOException e) {
+        	Log.e(TAG, "Error opening management interface");
+            VpnStatus.logException(e);
+        }
+        
+        mWorker = new UsernatWorker(this, argv, new HashMap<String, String>());
+		mWorkerThread = new Thread(mWorker, "Usernat worker");
+		mWorkerThread.start();
+
+		try {
+			mSocket = mServerSocket.accept();
+			InputStream instream = mSocket.getInputStream();
+            mServerSocket.close();
+
+            boolean prologueMode = true;
+            byte[] buffer = new byte[65536];
+            int msgLen = 0;
+            int pos = 0;
+            
+			while(true) {
+				if(prologueMode) {
+					int s1 = instream.read();
+					if(s1 == -1) {
+						break;
+					}
+					
+					int s2 = instream.read();
+					if(s2 == -1) {
+						break;
+					}
+					
+					msgLen = ((s1 & 0xff) << 8) & 0x0000ffff;
+					msgLen |= s2 & 0x000000ff;
+					
+					prologueMode = false;
+					pos = 0;
+				}
+				
+				int numbytesread = instream.read(buffer, pos, msgLen - pos);
+				if(numbytesread == -1) {
+					break;
+				}
+				
+				if(pos + numbytesread == msgLen) {
+					Log.d(socketName, new String(buffer, 0, msgLen));
+					prologueMode = true;
+				} else {
+					pos += numbytesread;
+				}
+			}
+		} catch (IOException e) {
+            if (!e.getMessage().equals("socket closed"))
+                VpnStatus.logException(e);
+		}
+		
+		mConnectionStatus = ConnectionStatus.TERMINATED;
 	}
 
 	@Override
 	protected String getThreadName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Usernat";
 	}
 
 	@Override
 	public void establishConnection(Map<String, Object> options) {
-		// TODO Auto-generated method stub
-
+		if(mConnectionStatus != ConnectionStatus.DISCONNECTED) {
+			return;
+		}
+		
+		mVpnTunnelCtx = initUsernatTun();
+		
+		startLoop();
 	}
 
 	@Override
 	public void terminateConnection() {
-		// TODO Auto-generated method stub
-
+		if(mConnectionStatus != ConnectionStatus.TERMINATED) {
+			mConnectionStatus = ConnectionStatus.TERMINATED;
+			deinitUsernatTun(mVpnTunnelCtx);
+			mVpnTunnelCtx = 0;
+		}
 	}
+	
+	private native long initUsernatTun();
+	private native void deinitUsernatTun(long tunctx);
 }
