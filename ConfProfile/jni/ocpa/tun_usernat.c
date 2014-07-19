a9c6dafcc078be461c84981d6816d1b21bf20784
Dmitry Vorobiev <vorobievds@gmail.com>
2014-07-19 14:04:33 +0400

    [~] tun_usernat was refactored to create special (captured but not VPNized) tunnels

--- ConfProfile/jni/ocpa/tun_usernat.c
+++ ConfProfile/jni/ocpa/tun_usernat.c
@@ -6,24 +6,40 @@
 
 #include <stdlib.h>
 #include <sys/socket.h>
-#include "tun_usernat.h"
+#include <errno.h>
+#include <unistd.h>
 
-usernat_tun_ctx_t* usernat_tun_init() {
+#include "android_log_utils.h"
+#include "android_jni.h"
+#include "tun_usernat.h"
+#include "protoheaders.h"
+#include "sockaddrs.h"
+#include "router.h"
+
+#define LOG_TAG "tun_usernat.c"
+
+static nat_link_t* link_init();
+static void link_deinit(nat_link_t* link);
+static nat_link_t* find_link(usernat_tun_ctx_t* ctx, ocpa_ip_packet_t* packet);
+static nat_link_t* find_link4(nat_link_t* root_link, uint32_t local_addr, uint16_t local_port, uint32_t remote_addr, uint16_t remote_port);
+
+usernat_tun_ctx_t* usernat_tun_init(jobject jtun_instance) {
 	usernat_tun_ctx_t* ctx = malloc(sizeof(usernat_tun_ctx_t));
 	if(ctx == NULL) {
 		return NULL;
 	}
 
-	int fds[2];
-	if(socketpair(AF_UNIX, SOCK_STREAM, PF_UNSPEC, fds) != 0) {
-		free(ctx);
-		return NULL;
-	}
+	memset(ctx, 0, sizeof(usernat_tun_ctx_t));
+	common_tun_set((common_tun_ctx_t*) ctx, jtun_instance);
 
-	common_tun_set((common_tun_ctx_t*) ctx);
 
-	ctx->common.local_fd = fds[0];
-	ctx->common.remote_fd = fds[1];
+	ctx->common.send_func = usernat_tun_send;
+	ctx->common.recv_func = usernat_tun_recv;
+
+	ctx->local4 = 0;
+	memset(ctx->local6, 0, sizeof(ctx->local6));
+	ctx->remote4 = 0;
+	memset(ctx->remote6, 0, sizeof(ctx->remote6));
 
 	return ctx;
 }
@@ -37,3 +53,265 @@ void usernat_tun_deinit(usernat_tun_ctx_t* ctx) {
 
 	free(ctx);
 }
+
+ssize_t usernat_tun_send(intptr_t tun_ctx, uint8_t* buff, int len) {
+	if(tun_ctx == (intptr_t) NULL) {
+		errno = EBADF;
+		return -1;
+	}
+
+	//we received this packet from TUN device
+	LOGD(LOG_TAG, "usernat_tun_send() called");
+	usernat_tun_ctx_t* ctx = (usernat_tun_ctx_t*) tun_ctx;
+
+	ocpa_ip_packet_t packet;
+	memset(&packet, 0, sizeof(packet));
+	packet.buff = buff;
+	packet.buff_len = len;
+	ip_parse_packet(&packet);
+
+	nat_link_t* link = find_link(ctx, &packet);
+	if(link == NULL) {
+		//this packet is invalid, drop it
+		return 0;
+	}
+
+	pthread_rwlock_rdlock(ctx->common.router_ctx->rwlock4);
+	if(packet.ipver == 4) {
+		if(ip4_addr_eq(ctx->local4, ntohl(packet.ip_header.v4->saddr))) {
+			//outgoing usernat packet
+			packet.ip_header.v4->saddr = htonl(ctx->remote4);
+			packet.ip_header.v4->daddr = htonl(ctx->local4);
+		} else if(ip4_addr_eq(ctx->remote4, ntohl(packet.ip_header.v4->daddr))) {
+			//incoming usernat packet
+			packet.ip_header.v4->saddr = htonl(link->tcp4.real_dst_addr);
+			packet.ip_header.v4->daddr = htonl(ctx->local4);
+		}
+
+		ip4_calc_ip_checksum(buff, len);
+
+		switch(packet.payload_proto) {
+		case IPPROTO_TCP: {
+			ip4_calc_tcp_checksum(buff, len);
+			break;
+		}
+		case IPPROTO_UDP: {
+			ip4_calc_udp_checksum(buff, len);
+			break;
+		}
+		default: {
+			LOGE(LOG_TAG, "Can't calculate checksum for protocol %d", packet.payload_proto);
+			break;
+		}
+		}
+	} else if(packet.ipver == 6) {
+
+	}
+
+	//TODO: calculate traffic
+	//ctx->router_ctx->dev_tun_ctx.bytes_out += res;
+
+	//start capture
+	pcap_output_write(ctx->common.pcap_output, buff, 0, len);
+	//end capture
+
+
+	len = write(ctx->common.router_ctx->dev_tun_ctx.local_fd, buff, len);
+	pthread_rwlock_unlock(ctx->common.router_ctx->rwlock4);
+	return len;
+}
+
+ssize_t usernat_tun_recv(intptr_t tun_ctx, uint8_t* buff, int len) {
+	//this function should never be called
+	LOGE(LOG_TAG, "usernat_tun_recv() called");
+	errno = EBADF;
+	return -1;
+}
+
+static nat_link_t* link_init() {
+	nat_link_t* result = malloc(sizeof(nat_link_t));
+	if(result != NULL) {
+		memset(result, 0, sizeof(nat_link_t));
+	}
+
+	return result;
+}
+
+static void link_deinit(nat_link_t* link) {
+	if(link == NULL) {
+		return;
+	}
+
+	memset(link, 0, sizeof(nat_link_t));
+	free(link);
+}
+
+static nat_link_t* find_link(usernat_tun_ctx_t* ctx, ocpa_ip_packet_t* packet) {
+	if(ctx == NULL || packet == NULL || packet->ip_header.raw == NULL) {
+		return NULL;
+	}
+
+	bool is_incoming = false;
+	nat_link_t* result = NULL;
+
+	uint16_t local_port = 0;
+	uint16_t remote_port = 0;
+	sockaddr_uni tmp_sa;
+	JNIEnv* jnienv;
+
+	if(packet->ipver == 4) {
+		uint32_t remote_addr = 0;
+		if(ip4_addr_eq(ctx->local4, ntohl(packet->ip_header.v4->saddr))) {
+			//outgoing usernat packet
+			is_incoming = false;
+			local_port = packet->src_port;
+			remote_addr = ntohl(packet->ip_header.v4->daddr);
+			remote_port = packet->dst_port;
+		} else if(ip4_addr_eq(ctx->remote4, ntohl(packet->ip_header.v4->daddr))) {
+			//incoming usernat packet
+			is_incoming = true;
+			local_port = packet->dst_port;
+			remote_addr = ctx->remote4;
+			remote_port = packet->src_port;
+		} else {
+			//stray packet
+			return NULL;
+		}
+
+		nat_link_t* root_link = NULL;
+		uint8_t local_link_type;
+		if(packet->payload_proto == IPPROTO_TCP) {
+			root_link = ctx->tcp4;
+			local_link_type = NAT_LINK_TCP4;
+		} else if(packet->payload_proto == IPPROTO_UDP) {
+			//TODO: UDP
+			local_link_type = NAT_LINK_UDP4;
+		} else {
+			return NULL;
+		}
+
+		result = find_link4(root_link, ctx->local4, local_port, remote_addr, remote_port);
+
+		if(result == NULL) {
+			if(is_incoming) {
+				//drop incoming packet if link doesn't exist
+				//this is a NAT
+				return NULL;
+			}
+
+			//not found, create a new one
+			result = link_init();
+			if(result == NULL) {
+				//not enough memory
+				return NULL;
+			}
+
+			result->common.link_type = local_link_type;
+			switch(local_link_type) {
+			case NAT_LINK_TCP4: {
+				if(packet->payload_header.tcp->syn == 0) {
+					//create a link on SYN packet only
+					free(result);
+					return NULL;
+				}
+
+				result->tcp4.real_src_addr = ntohl(packet->ip_header.v4->saddr);
+				result->tcp4.real_src_port = ntohs(packet->payload_header.tcp->source);
+				result->tcp4.real_dst_addr = ntohl(packet->ip_header.v4->daddr);
+				result->tcp4.real_dst_port = ntohs(packet->payload_header.tcp->dest);
+
+				result->common.sock_accept = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+				if(result->common.sock_accept == -1) {
+					free(result);
+					return NULL;
+				}
+
+				if(bind(result->common.sock_accept, &tmp_sa.sa, sizeof(tmp_sa)) == -1) {
+					free(result);
+					return NULL;
+				}
+
+				result->common.sock_connect = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+				if(result->common.sock_connect == -1) {
+					close(result->common.sock_accept);
+					free(result);
+					return NULL;
+				}
+
+				break;
+			}
+			case NAT_LINK_UDP4: {
+				result->common.sock_accept = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+				if(result->common.sock_accept == -1) {
+					free(result);
+					return NULL;
+				}
+
+				if(bind(result->common.sock_accept, &tmp_sa.sa, sizeof(tmp_sa)) == -1) {
+					free(result);
+					return NULL;
+				}
+
+				result->common.sock_connect = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+				if(result->common.sock_connect == -1) {
+					close(result->common.sock_accept);
+					free(result);
+					return NULL;
+				}
+				break;
+			}
+			default: {
+				//unsupported link type, should never be happen
+				free(result);
+				return NULL;
+			}
+			}
+
+			result->common.hop_port = tmp_sa.in.sin_port;
+
+			//TODO: init sockets, add a link
+			bool need_detach = androidjni_attach_thread(&jnienv);
+
+			jboolean is_protected = JNI_FALSE;
+			if(ctx->common.jni._tun_instance != NULL) {
+				jobject instance = ctx->common.jni._tun_instance;
+				jmethodID methodID = ctx->common.jni._method_protect;
+				is_protected = (*jnienv)->CallBooleanMethod(jnienv,
+						instance,
+						methodID,
+						result->common.sock_connect);
+				if((*jnienv)->ExceptionOccurred(jnienv)){
+					(*jnienv)->ExceptionDescribe(jnienv);
+					(*jnienv)->ExceptionClear(jnienv);
+					free(result);
+					result = NULL;
+				}
+			}
+
+			if(is_protected == JNI_FALSE && result != NULL) {
+				free(result);
+				result = NULL;
+			}
+
+			if(need_detach) {
+				androidjni_detach_thread();
+			}
+
+		}
+
+	} else if(packet->ipver == 6) {
+		//TODO: implement IPv6
+	} else {
+		LOGE(LOG_TAG, "Invalid IP version (supported versions: IPv4, IPv6)");
+		return NULL;
+	}
+
+	return result;
+}
+
+static nat_link_t* find_link4(nat_link_t* root_link, uint32_t local_addr, uint16_t local_port, uint32_t remote_addr, uint16_t remote_port) {
+	if(root_link == NULL) {
+		return NULL;
+	}
+}
+
--- ConfProfile/jni/ocpa/tun_usernat.h
+++ ConfProfile/jni/ocpa/tun_usernat.h
@@ -9,13 +9,79 @@
 
 #include "tun.h"
 
+#define NAT_LINK_IP6  2
+#define NAT_LINK_TCP4 0
+#define NAT_LINK_UDP4 1
+#define NAT_LINK_TCP6 (NAT_LINK_TCP4 | NAT_LINK_IP6)
+#define NAT_LINK_UDP6 (NAT_LINK_UDP4 | NAT_LINK_IP6)
+
 typedef struct usernat_tun_ctx_t usernat_tun_ctx_t;
+typedef struct nat_common_link_t nat_common_link_t;
+typedef struct nat_tcp4_link_t nat_tcp4_link_t;
+typedef struct nat_udp4_link_t nat_udp4_link_t;
+typedef struct nat_tcp6_link_t nat_tcp6_link_t;
+typedef struct nat_udp6_link_t nat_udp6_link_t;
+typedef union nat_link_t nat_link_t;
+
+struct nat_common_link_t {
+	uint8_t link_type;
+	int sock_accept;
+	int sock_connect;
+	pid_t socat_pid;
+	uint16_t hop_port;
+};
+
+struct nat_tcp4_link_t {
+	uint8_t link_type;
+	int sock_accept;
+	int sock_connect;
+	pid_t socat_pid;
+	uint16_t hop_port;
+
+    uint32_t real_src_addr;
+    uint16_t real_src_port;
+    uint32_t real_dst_addr;
+    uint16_t real_dst_port;
+
+    nat_tcp4_link_t* next;
+};
+
+struct nat_tcp6_link_t {
+	uint8_t link_type;
+	int sock_accept;
+	int sock_connect;
+	pid_t socat_pid;
+	uint16_t hop_port;
+
+    uint8_t real_src_addr[16];
+    uint16_t real_src_port;
+    uint8_t real_dst_addr[16];
+    uint16_t real_dst_port;
+
+    nat_tcp6_link_t* next;
+};
+
+union nat_link_t {
+	nat_common_link_t common;
+	nat_tcp4_link_t tcp4;
+	nat_tcp6_link_t tcp6;
+};
 
 struct usernat_tun_ctx_t {
 	common_tun_ctx_t common;
+	uint32_t local4;
+	uint8_t local6[16];
+	uint32_t remote4;
+	uint8_t remote6[16];
+
+	nat_link_t* tcp4;
+	nat_link_t* tcp6;
 };
 
-usernat_tun_ctx_t* usernat_tun_init();
+usernat_tun_ctx_t* usernat_tun_init(jobject jtun_instance);
 void usernat_tun_deinit(usernat_tun_ctx_t* ctx);
+
+ssize_t usernat_tun_send(intptr_t tun_ctx, uint8_t* buff, int len);
+ssize_t usernat_tun_recv(intptr_t tun_ctx, uint8_t* buff, int len);
 
 #endif /* TUN_USERNAT_H_ */
