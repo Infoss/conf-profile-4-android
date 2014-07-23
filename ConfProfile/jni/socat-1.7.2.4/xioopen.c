beb0c61599a85d2a28c65921ddebb96edba66507
Dmitry Vorobiev <vorobievds@gmail.com>
2014-07-23 22:31:05 +0400

    [*] fixed mistype

--- ConfProfile/jni/socat-1.7.2.4/xioopen.c
+++ ConfProfile/jni/socat-1.7.2.4/xioopen.c
@@ -177,7 +177,7 @@ const struct addrname addressnames[] = {
 #if WITH_LISTEN
    { "socket-listen",		&xioaddr_socket_listen },
 #ifdef OCPA_CHANGES
-   { "socket-fd-acept",		&xioaddr_socket_fd_accept },
+   { "socket-fd-accept",		&xioaddr_socket_fd_accept },
 #endif /* OCPA_CHANGES */
 #endif /* WITH_LISTEN */
    { "socket-recv",		&xioaddr_socket_recv },
