ab122e291511442af62892d23eb9aa2eec1cb2b1
Dmitry Vorobiev <vorobievds@gmail.com>
2014-08-04 20:22:51 +0400

    [+] added methods from AOSP libnativehelper for printing java exceptions from native code

--- ConfProfile/jni/ocpa/android_log_utils.h
+++ ConfProfile/jni/ocpa/android_log_utils.h
@@ -9,6 +9,7 @@
 
 #include <android/log.h>
 #include <stdint.h>
+#include <jni.h>
 
 #define LOGV(LOG_TAG, ...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 #define LOGD(LOG_TAG, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
@@ -18,5 +19,6 @@
 #define LOGF(LOG_TAG, ...) __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, __VA_ARGS__)
 
 inline void log_dump_packet(const char *tag, uint8_t* buff, int len);
+void jniLogException(JNIEnv* env, int priority, const char* tag, jthrowable exception);
 
 #endif /* ANDROID_LOG_UTILS_H_ */
