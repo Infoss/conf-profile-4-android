9fd9ce58ed1a5dbb07f7e5fbdca5e4d368ea80b0
Dmitry Vorobiev <vorobievds@gmail.com>
2014-08-11 21:10:24 +0400

    [~] base class of list item now supports layout id for a view instantiation and view id for checking convertView

--- ConfProfile/src/no/infoss/confprofile/profile/data/ListItem.java
+++ ConfProfile/src/no/infoss/confprofile/profile/data/ListItem.java
@@ -3,6 +3,8 @@ package no.infoss.confprofile.profile.data;
 public class ListItem {
 	private String mMainText;
 	private String mSubText;
+	private int mLayoutId;
+	private int mRootViewId;
 	
 	public ListItem() {
 		
@@ -29,5 +31,29 @@ public class ListItem {
 	
 	public void setSubText(String subText) {
 		this.mSubText = subText;
+	}
+
+	public int getLayoutId() {
+		return mLayoutId;
+	}
+
+	/**
+	 * Set layout id which is used when new view should be instantiated.
+	 * @param layoutId
+	 */
+	public void setLayoutId(int layoutId) {
+		this.mLayoutId = layoutId;
+	}
+
+	public int getRootViewId() {
+		return mRootViewId;
+	}
+
+	/**
+	 * Set root view id as hint for checking is provided view can be converted into current item view. 
+	 * @param rootViewId
+	 */
+	public void setRootViewId(int rootViewId) {
+		this.mRootViewId = rootViewId;
 	}
 }
