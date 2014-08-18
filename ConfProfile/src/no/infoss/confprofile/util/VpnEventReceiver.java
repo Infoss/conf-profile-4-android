afd283d55f4aed98a8db120cb272d286171506ff
Dmitry Vorobiev <vorobievds@gmail.com>
2014-08-18 19:42:13 +0400

    [~] improved to match tunnel state object

--- ConfProfile/src/no/infoss/confprofile/util/VpnEventReceiver.java
+++ ConfProfile/src/no/infoss/confprofile/util/VpnEventReceiver.java
@@ -1,5 +1,7 @@
 package no.infoss.confprofile.util;
 
+import java.util.Date;
+
 import no.infoss.confprofile.vpn.VpnManagerInterface;
 import android.content.Context;
 import android.content.Intent;
@@ -32,12 +34,28 @@ public class VpnEventReceiver extends BroadcastReceiverEx<VpnEventReceiver.VpnEv
 			int state = intent.getIntExtra(
 					VpnManagerInterface.KEY_TUNNEL_STATE, 
 					VpnManagerInterface.TUNNEL_STATE_DISCONNECTED);
-			getListener().onReceivedTunnelState(tunnelId, state, true);
+			Date date = (Date) intent.getSerializableExtra(VpnManagerInterface.KEY_EVENT_DATE);
+			String serverName = intent.getStringExtra(VpnManagerInterface.KEY_SERVER_NAME);
+			String remoteAddress = intent.getStringExtra(VpnManagerInterface.KEY_REMOTE_ADDRESS);
+			String localAddress = intent.getStringExtra(VpnManagerInterface.KEY_LOCAL_ADDRESS);
+			getListener().onReceivedTunnelState(tunnelId, 
+					state, 
+					date, 
+					serverName, 
+					remoteAddress, 
+					localAddress, 
+					true);
 		}
 	}
 
 	public interface VpnEventListener {
 		public void onReceivedServiceState(int state, boolean isBroadcast);
-		public void onReceivedTunnelState(String tunnelId, int state, boolean isBroadcast);
+		public void onReceivedTunnelState(String tunnelId, 
+				int state, 
+				Date date,
+				String serverName,
+				String remoteAddress,
+				String localAddress,
+				boolean isBroadcast);
 	}
 }
