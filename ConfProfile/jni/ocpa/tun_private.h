2a894565e0d72cbd4e84674b841c3c2edc279950
Dmitry Vorobiev <vorobievds@gmail.com>
2014-08-22 23:50:16 +0400

    [~] header with extracted private interface for tunnels

---
+++ ConfProfile/jni/ocpa/tun_private.h
@@ -0,0 +1,35 @@
+/*
+ * tun_private.h
+ *
+ *      Author: Dmitry Vorobiev
+ */
+
+#ifndef TUN_PRIVATE_H_
+#define TUN_PRIVATE_H_
+
+#include "tun.h"
+
+struct tun_ctx_private_t {
+	tun_ctx_t public;
+
+	refcount_t __ref_count;
+
+	pthread_rwlock_t* rwlock;
+	int local_fd;  //router side
+	int remote_fd; //vpn implementation side
+	uint32_t masquerade4;
+	uint8_t masquerade6[16];
+	bool use_masquerade4;
+	bool use_masquerade6;
+	router_ctx_t* router_ctx;
+
+	uint64_t bytes_in;
+	uint64_t bytes_out;
+
+	java_VpnTunnel* j_vpn_tun;
+
+	//Debug features
+	pcap_output_t* pcap_output;
+};
+
+#endif /* TUN_PRIVATE_H_ */
