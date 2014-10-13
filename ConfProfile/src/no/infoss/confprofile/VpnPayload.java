7c4d78e5c1b84ac6294fdc7f3ece6f66c9cc5d33
Dmitry Vorobiev <vorobievds@gmail.com>
2014-10-13 10:40:09 +0400

    [+] added activity for vpn payload details [+] added fragment for vpn payload details

---
+++ ConfProfile/src/no/infoss/confprofile/VpnPayload.java
@@ -0,0 +1,31 @@
+package no.infoss.confprofile;
+
+import no.infoss.confprofile.fragment.VpnPayloadFragment;
+import no.infoss.confprofile.profile.VpnDataCursorLoader;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class VpnPayload extends Activity {
+	public static final String TAG = VpnPayload.class.getSimpleName();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.profile_payloads);
+		
+		Intent intent = getIntent();
+		if(intent != null) {
+			FragmentManager fman = getFragmentManager();
+			VpnPayloadFragment f = (VpnPayloadFragment) fman.findFragmentById(R.id.fragmentVpnPayload);
+			if(f == null || !f.isAdded()) {
+				finish();
+				return;
+			}
+			
+			f.setProfileId(intent.getStringExtra(VpnDataCursorLoader.P_PAYLOAD_UUID));
+		}		
+	}
+}
+
