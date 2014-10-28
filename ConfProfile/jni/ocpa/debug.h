40e67ec9e4f9ca33cd810a5440bed23278c82e97
Dmitry Vorobiev <vorobievds@gmail.com>
2014-10-28 11:26:08 +0300

    [+] updated debug macro  (separate for debug and release versions)

--- ConfProfile/jni/ocpa/debug.h
+++ ConfProfile/jni/ocpa/debug.h
@@ -7,6 +7,11 @@
 #ifndef OCPA_DEBUG_H_
 #define OCPA_DEBUG_H_
 
+#ifdef __ANDROID__
+
+#ifndef NDEBUG
+
+//debug mode
 #define JNI_CORE_DEBUG 1
 #define STRONGSWAN_DEBUG 1
 #define TUN_DEBUG 1
@@ -14,5 +19,20 @@
 #define ROUTER_DEBUG 1
 #define ROUTER_EPOLL_DEBUG 0
 #define TRACE_DEBUG 1
+
+#else
+
+//release mode
+#define JNI_CORE_DEBUG 0
+#define STRONGSWAN_DEBUG 0
+#define TUN_DEBUG 0
+#define REFS_DEBUG 0
+#define ROUTER_DEBUG 0
+#define ROUTER_EPOLL_DEBUG 0
+#define TRACE_DEBUG 0
+
+#endif /* NDEBUG */
+
+#endif /* __ANDROID__ */
 
 #endif /* DEBUG_H_ */
