ce766256d6f77c282999fc61f9de29158dbb19b6
Dmitry Vorobiev <vorobievds@gmail.com>
2014-11-06 15:15:58 +0300

    [+] aaded abstract methods for entity self-mapping

--- ConfProfile/src/no/infoss/confprofile/entity/AppEntity.java
+++ ConfProfile/src/no/infoss/confprofile/entity/AppEntity.java
@@ -1,5 +1,11 @@
 package no.infoss.confprofile.entity;
 
-public abstract class AppEntity {
+import no.infoss.confprofile.format.ConfigurationProfile.Payload;
+import android.content.ContentValues;
+import android.database.Cursor;
 
+public abstract class AppEntity {
+	public abstract void mapCursor(Cursor cursor);
+	public abstract void mapPayload(Payload payload);
+	public abstract ContentValues asContentValues();
 }
