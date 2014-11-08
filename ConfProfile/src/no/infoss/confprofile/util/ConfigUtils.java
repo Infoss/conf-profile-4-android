70dfa762d067127a06b10a3f73a29a6c9edde000
Dmitry Vorobiev <vorobievds@gmail.com>
2014-11-08 23:08:35 +0300

    [+] added method for merging dicts [+] added constat and method for integer representation of vpn type

--- ConfProfile/src/no/infoss/confprofile/util/ConfigUtils.java
+++ ConfProfile/src/no/infoss/confprofile/util/ConfigUtils.java
@@ -9,12 +9,19 @@ import no.infoss.confprofile.format.Plist.Dictionary;
 import no.infoss.confprofile.format.VpnPayload;
 import no.infoss.confprofile.format.json.BuiltinTypeAdapterFactory;
 import no.infoss.confprofile.vpn.NetworkConfig;
+import no.infoss.confprofile.vpn.OpenVpnTunnel;
 import android.content.Context;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class ConfigUtils {
+	public static final int VPN_UNKNOWN = 0;
+	public static final int VPN_OPENVPN = 1;
+	public static final int VPN_IPSEC = 2;
+	public static final int VPN_L2TP = 3;
+	public static final int VPN_PPTP = 4;
+	
 	public static NetworkConfig buildNetworkConfig(VpnPayload payload) {
 		NetworkConfig config = new NetworkConfig();
 		
@@ -30,6 +37,26 @@ public class ConfigUtils {
 		}
 		
 		return config;
+	}
+	
+	public static int getVpnTypeCode(VpnPayload payload) {
+		int value = VPN_UNKNOWN;
+		
+		if(payload != null) {
+			if(VpnPayload.VPN_TYPE_PPTP.equals(payload.getVpnType())) {
+				value = VPN_PPTP;
+			} else if(VpnPayload.VPN_TYPE_L2TP.equals(payload.getVpnType())) {
+				value = VPN_L2TP;
+			} else if(VpnPayload.VPN_TYPE_IPSEC.equals(payload.getVpnType())) {
+				value = VPN_IPSEC;
+			} else if(VpnPayload.VPN_TYPE_CUSTOM.equals(payload.getVpnType())) {
+				if(OpenVpnTunnel.VPN_TYPE.equals(payload.getVpnSubType())) {
+					value = VPN_OPENVPN;
+				}
+			}
+		}
+		
+		return value;
 	}
 	
 	public static String extractOnDemandRules(VpnPayload payload) {
@@ -111,6 +138,19 @@ public class ConfigUtils {
 		}
 		
 		return result;
+	}
+	
+	public static Dictionary mergeDicts(Dictionary... dicts) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		for(Dictionary dict : dicts) {
+			if(dict == null) {
+				continue;
+			}
+			
+			map.putAll(dict.asMap());
+		}
+		
+		return Dictionary.wrap(map);
 	}
 	
 }
