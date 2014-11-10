b4c25a5e20314a2606eddc5a7d6e9512a00a022d
Dmitry Vorobiev <vorobievds@gmail.com>
2014-11-10 21:33:11 +0300

    [+] VpnPayloadFragment is able to show data for now

--- ConfProfile/src/no/infoss/confprofile/fragment/VpnPayloadFragment.java
+++ ConfProfile/src/no/infoss/confprofile/fragment/VpnPayloadFragment.java
@@ -4,32 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import no.infoss.confprofile.R;
-import no.infoss.confprofile.format.VpnPayload;
+import no.infoss.confprofile.model.common.ListItemModel;
+import no.infoss.confprofile.model.vpndata.CertificateModel;
+import no.infoss.confprofile.model.vpndata.IpsecGroupNameModel;
+import no.infoss.confprofile.model.vpndata.LoginModel;
+import no.infoss.confprofile.model.vpndata.OverridePrimaryModel;
+import no.infoss.confprofile.model.vpndata.PasswordModel;
+import no.infoss.confprofile.model.vpndata.PptpEncryptionModel;
+import no.infoss.confprofile.model.vpndata.RemoteServerModel;
+import no.infoss.confprofile.model.vpndata.RsaSecurIdModel;
+import no.infoss.confprofile.model.vpndata.SharedSecretModel;
 import no.infoss.confprofile.profile.BaseQueryCursorLoader;
 import no.infoss.confprofile.profile.DbOpenHelper;
-import no.infoss.confprofile.profile.PayloadInfoExLoader;
-import no.infoss.confprofile.profile.PayloadsCursorLoader;
-import no.infoss.confprofile.profile.data.PayloadInfoEx;
-import no.infoss.confprofile.util.ConfigUtils;
+import no.infoss.confprofile.profile.VpnDataCursorLoader;
+import no.infoss.confprofile.profile.data.VpnData;
+import no.infoss.confprofile.util.ListItemMapper;
 import android.app.ActionBar;
+import android.app.ActionBar.Tab;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
-import android.app.ActionBar.Tab;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.litecoding.classkit.view.HeaderObjectAdapter;
-import com.litecoding.classkit.view.HeaderObjectAdapter.HeaderObjectMapper;
 
-public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<List<List<PayloadInfoEx>>>  {
+public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<Cursor>  {
 	public static final String TAG = VpnPayloadFragment.class.getSimpleName();
 	
 	public static final String TAB_L2TP = "l2tp";
@@ -37,11 +43,23 @@ public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<List
 	public static final String TAB_IPSEC = "ipsec";
 	public static final String TAB_OPENVPN = "openvpn";
 	
-	private final List<List<PayloadInfoEx>> mPayloads = new ArrayList<List<PayloadInfoEx>>();
-	private HeaderObjectAdapter<PayloadInfoEx, List<PayloadInfoEx>> mAdapter;
+	private static final List<String> SINGLE_EMPTY_HEADER_LIST = new ArrayList<String>(1);
+	static {
+		SINGLE_EMPTY_HEADER_LIST.clear();
+		SINGLE_EMPTY_HEADER_LIST.add("");
+	}
+	
+	private final List<List<ListItemModel<?>>> mItems = new ArrayList<List<ListItemModel<?>>>();
+	private final List<ListItemModel<?>> mPptpItems = new ArrayList<ListItemModel<?>>(6);
+	private final List<ListItemModel<?>> mL2tpItems = new ArrayList<ListItemModel<?>>(6);
+	private final List<ListItemModel<?>> mIpsecItems = new ArrayList<ListItemModel<?>>(6);
+	private final List<ListItemModel<?>> mOpenvpnItems = new ArrayList<ListItemModel<?>>(6);
+	private HeaderObjectAdapter<ListItemModel<?>, String> mAdapter;
 	
 	private DbOpenHelper mDbHelper;
 	private String mPayloadUuid;
+	private VpnData mData = null;
+	
 	
 	public VpnPayloadFragment() {
 		super();
@@ -68,20 +86,17 @@ public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<List
 			
 			@Override
 			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-				// TODO Auto-generated method stub
-				
+				//nothing to do here
 			}
 			
 			@Override
 			public void onTabSelected(Tab tab, FragmentTransaction ft) {
-				// TODO Auto-generated method stub
 				VpnPayloadFragment.this.onTabSelected(tab.getTag());
 			}
 			
 			@Override
 			public void onTabReselected(Tab tab, FragmentTransaction ft) {
-				// TODO Auto-generated method stub
-				
+				//nothing to do here
 			}
 		};
 		
@@ -113,62 +128,12 @@ public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<List
 			mDbHelper = DbOpenHelper.getInstance(activity);
 		}
 		
-		mAdapter = new HeaderObjectAdapter<PayloadInfoEx, List<PayloadInfoEx>>(activity.getLayoutInflater(), 
-				mPayloads, 
+		mAdapter = new HeaderObjectAdapter<ListItemModel<?>, String>(activity.getLayoutInflater(), 
+				SINGLE_EMPTY_HEADER_LIST, 
 				R.layout.simple_list_item_1_header, 
-				mPayloads, 
-				R.layout.payload_item, 
-				new HeaderObjectMapper<PayloadInfoEx, List<PayloadInfoEx>>() {
-
-					@Override
-					public View prepareView(int position, View convertView, PayloadInfoEx data) {
-						return null;
-					}
-					
-					@Override
-					public void mapData(int position, View view, PayloadInfoEx data) {
-						ImageView imageView = null;
-						
-						imageView = (ImageView) view.findViewById(R.id.icon);
-						if(imageView != null) {
-							imageView.setImageResource(R.drawable.profiles);
-						}
-						
-						TextView textView = null;
-						
-						textView = (TextView) view.findViewById(R.id.payloadName);
-						if(textView != null) {
-							textView.setText(data.payload.getPayloadDisplayName());
-						}
-						
-						if(data.payload instanceof VpnPayload) {
-							textView = (TextView) view.findViewById(R.id.payloadDetails1);
-							if(textView != null) {
-								textView.setVisibility(View.GONE);
-							}
-							
-							textView = (TextView) view.findViewById(R.id.payloadDetails2);
-							if(textView != null) {
-								textView.setVisibility(View.GONE);
-							}
-						}
-					}
-
-					@Override
-					public View prepareHeaderView(int position, View convertView, List<PayloadInfoEx> header) {
-						return null;
-					}
-
-					@Override
-					public void mapHeader(int position, View view, List<PayloadInfoEx> header) {
-						Activity activity = getActivity();
-						if(activity != null && header.size() > 0) {
-							TextView textView = (TextView) view.findViewById(android.R.id.text1);
-							textView.setText(ConfigUtils.getPayloadNameByType(activity, header.get(0).payloadType));
-						}
-					}
-			
-				});
+				mItems, 
+				R.layout.payload_item,
+				new ListItemMapper(activity));
 	}
 
 	@Override
@@ -196,7 +161,7 @@ public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<List
 		
 		request = new Bundle();
 		request.putInt(BaseQueryCursorLoader.STMT_TYPE, BaseQueryCursorLoader.STMT_SELECT);
-		request.putString(BaseQueryCursorLoader.P_SELECT_BY, PayloadsCursorLoader.COL_PROFILE_ID);
+		request.putString(BaseQueryCursorLoader.P_SELECT_BY, VpnDataCursorLoader.COL_PAYLOAD_UUID);
 		request.putString(BaseQueryCursorLoader.P_SELECT_VALUE, mPayloadUuid);
 		getLoaderManager().initLoader(mPayloadUuid.hashCode(), request, this);
 	}
@@ -212,30 +177,81 @@ public class VpnPayloadFragment extends Fragment implements LoaderCallbacks<List
 	}
 
 	@Override
-	public Loader<List<List<PayloadInfoEx>>> onCreateLoader(int id, Bundle params) {
-		return new PayloadInfoExLoader(getActivity(), id, params, mDbHelper);
+	public Loader<Cursor> onCreateLoader(int id, Bundle params) {
+		return new VpnDataCursorLoader(getActivity(), id, params, mDbHelper);
 	}
 
 	@Override
-	public void onLoadFinished(Loader<List<List<PayloadInfoEx>>> loader, List<List<PayloadInfoEx>> data) {
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
 		showPayloadsDetails(data);
 	}
 
 	@Override
-	public void onLoaderReset(Loader<List<List<PayloadInfoEx>>> loader) {
+	public void onLoaderReset(Loader<Cursor> loader) {
 		// nothing to do here
 	}
 	
 	private void onTabSelected(Object tabTag) {
-		
+		if(TAB_PPTP.equals(tabTag)) {
+			mItems.clear();
+			mItems.add(mPptpItems);
+		} else if(TAB_L2TP.equals(tabTag)) {
+			mItems.clear();
+			mItems.add(mL2tpItems);
+		} else if(TAB_IPSEC.equals(tabTag)) {
+			mItems.clear();
+			mItems.add(mIpsecItems);
+		} else if(TAB_OPENVPN.equals(tabTag)) {
+			mItems.clear();
+			mItems.add(mOpenvpnItems);
+		}
+		mAdapter.notifyDataSetChanged();
 	}
 	
-	private void showPayloadsDetails(List<List<PayloadInfoEx>> data) {
-		if(data != null) {
-			mPayloads.clear();
-			mPayloads.addAll(data);
+	private void showPayloadsDetails(Cursor data) {
+		if(data != null && data.getCount() > 0) {
+			data.moveToFirst();
+			mData = new VpnData();
+			mData.mapCursor(data);
+			
+			rebuildLists();
 			mAdapter.notifyDataSetChanged();
-			//TODO: add onItemClickListener to the grid
 		}
+	}
+	
+	private void rebuildLists() {
+		ListItemModel<VpnData> remote = new RemoteServerModel(mData); 
+		ListItemModel<VpnData> login = new LoginModel(mData);
+		ListItemModel<VpnData> password = new PasswordModel(mData);
+		ListItemModel<VpnData> rsa = new RsaSecurIdModel(mData);
+		ListItemModel<VpnData> psk = new SharedSecretModel(mData);
+		ListItemModel<VpnData> override = new OverridePrimaryModel(mData);
+		ListItemModel<VpnData> cert = new CertificateModel(mData);
+		ListItemModel<VpnData> encryption = new PptpEncryptionModel(mData);
+		ListItemModel<VpnData> group = new IpsecGroupNameModel(mData);
+		
+		mPptpItems.clear();
+		mPptpItems.add(remote);
+		mPptpItems.add(login);
+		mPptpItems.add(rsa);
+		mPptpItems.add(password);
+		mPptpItems.add(encryption);
+		mPptpItems.add(override);
+		
+		mL2tpItems.clear();
+		mL2tpItems.add(remote);
+		mL2tpItems.add(login);
+		mL2tpItems.add(rsa);
+		mL2tpItems.add(password);
+		mL2tpItems.add(psk);
+		mL2tpItems.add(override);
+		
+		mIpsecItems.clear();
+		mIpsecItems.add(remote);
+		mIpsecItems.add(login);
+		mIpsecItems.add(password);
+		mIpsecItems.add(cert);
+		mIpsecItems.add(group);
+		mIpsecItems.add(psk);
 	}
 }
